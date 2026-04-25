92ad6902a519d6c65e88fb5418778628f9014d3a
ThanaponJaiauea <162958446+ThanaponJaiauea@users.noreply.github.com>
2026-04-25 19:46:51 +0700

    #

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,18 +1,37 @@
-import { defineConfig } from 'prisma/config'
-import 'dotenv/config'
+// import { defineConfig } from 'prisma/config'
+// import 'dotenv/config'
+
+// export default defineConfig({
+//   earlyAccess: true,
+//   schema: 'prisma/schema',
+//   datasource: {
+//     url: process.env.DATABASE_URL,
+//   },
+//   migrate: {
+//     async adapter() {
+//       const { PrismaPg } = await import('@prisma/adapter-pg')
+//       return new PrismaPg({
+//         connectionString: process.env.DATABASE_URL,
+//       })
+//     }
+//   }
+// })
+
+import { defineConfig } from "prisma/config";
+import "dotenv/config";
 
 export default defineConfig({
   earlyAccess: true,
-  schema: 'prisma/schema',
+  schema: "prisma/schema",
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DIRECT_URL,
   },
   migrate: {
     async adapter() {
-      const { PrismaPg } = await import('@prisma/adapter-pg')
+      const { PrismaPg } = await import("@prisma/adapter-pg");
       return new PrismaPg({
-        connectionString: process.env.DATABASE_URL,
-      })
-    }
-  }
-})
+        connectionString: process.env.DIRECT_URL,
+      });
+    },
+  },
+});
