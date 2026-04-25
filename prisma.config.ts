f7a1b4aa19e6ae3216d87433a7bd05ac9312964b
ThanaponJaiauea <162958446+ThanaponJaiauea@users.noreply.github.com>
2026-04-25 19:58:53 +0700

    edit error migrate

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,35 +1,8 @@
-// import { defineConfig } from 'prisma/config'
-// import 'dotenv/config'
-
-// export default defineConfig({
-//   earlyAccess: true,
-//   schema: 'prisma/schema',
-//   datasource: {
-//     url: process.env.DATABASE_URL,
-//   },
-//   migrate: {
-//     async adapter() {
-//       const { PrismaPg } = await import('@prisma/adapter-pg')
-//       return new PrismaPg({
-//         connectionString: process.env.DATABASE_URL,
-//       })
-//     }
-//   }
-// })
-
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema",
   datasource: {
     url: process.env.DIRECT_URL,
-  },
-  migrate: {
-    async adapter() {
-      const { PrismaPg } = await import("@prisma/adapter-pg");
-      return new PrismaPg({
-        connectionString: process.env.DIRECT_URL,
-      });
-    },
   },
 });
